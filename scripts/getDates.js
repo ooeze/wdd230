6a722d8db4bb115c07ea806c8974f6aedcc6c6eb
ooezenwa <ezenwa1994@gmail.com>
2025-01-19 00:34:45 +0100

    first commit

---
+++ scripts/getDates.js
@@ -0,0 +1,6 @@
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+
+const lastModifiedDate = document.lastModified;
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModifiedDate}`;
