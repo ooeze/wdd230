146f471ab0e457a1d10badd478d9ceb541d7e882
ooezenwa <ezenwa1994@gmail.com>
2025-02-15 00:42:42 +0100

    comit

--- chamber/scripts/hambugger.js
+++ chamber/scripts/hambugger.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var hamburgerMenu = document.getElementById('hamburger-menu');
     var mobileNav = document.getElementById('mobile-nav');
 
-    hamburgerMenu.addEventListener('click', function() {
+    hamburgerMenu.addEventListener('click', function () {
         if (mobileNav.style.display === 'flex') {
             mobileNav.style.display = 'none';
+            hamburgerMenu.classList.remove('open');
         } else {
             mobileNav.style.display = 'flex';
+            hamburgerMenu.classList.add('open');
         }
     });
 });
